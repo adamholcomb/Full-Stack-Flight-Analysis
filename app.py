dd1e5fc41de9f02d0ba196c143265f2edc477840
adamholcomb <adam.holcomb@ymail.com>
2021-08-11 20:18:25 -0500

    Finish Flask

---
+++ app.py
@@ -0,0 +1,59 @@
+# Import dependecies
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+from flask import Flask
+
+########################################################################
+# Configure database connection
+rds_string = "postgresql://postgres:postgres@localhost:5432/flight_db"
+engine = create_engine(rds_string)
+conn = engine.connect()
+
+########################################################################
+# Configure Flask server
+app = Flask(__name__)
+
+########################################################################
+# Build routes
+@app.route("/visualization1")
+def test1():
+    data = pd.read_sql("SELECT * FROM flight", conn)
+
+    # ADD CODE HERE TO FORMAT DATA FOR VISUALIZATION 1
+    data = data[['origin_name','dest_home','min_price_usd','origin_gps','dest_gps']]
+
+    return(data.to_json())
+
+@app.route("/visualization2")
+def test2():
+    data = pd.read_sql("SELECT * FROM flight", conn)
+
+
+    # ADD CODE HERE TO FORMAT DATA FOR VISUALIZATION 2
+    data = data[['origin_name','dest_home','min_price_usd']]
+
+    return(data.to_json())
+
+@app.route("/visualization3")
+def test3():
+    data = pd.read_sql("SELECT * FROM flight", conn)
+
+
+    # ADD CODE HERE TO FORMAT DATA FOR VISUALIZATION 3
+    data = data[['carrier','min_price_usd']]
+    test_df = data[['carrier','min_price_usd']]
+    test_df1 = test_df.groupby('carrier').min()
+
+    test_df2 = test_df.groupby('carrier').count()
+
+    carrier_df = pd.merge(test_df1,test_df2,how="inner",on="carrier")
+    carrier_df = carrier_df.rename({'min_price_usd_x': 'min_cost', 'min_price_usd_y': 'flight_count'}, axis='columns')
+    data = carrier_df
+    
+    return(data.to_json())
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
