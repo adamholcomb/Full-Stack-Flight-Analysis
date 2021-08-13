06a825d43f20fccfa5287dcaf2da8f56b1ef697a
Simon Castellanos <simoncastellanos9@gmail.com>
2021-08-13 10:04:11 -0500

    list of origins

--- logic3.js
+++ logic3.js
@@ -6,6 +6,9 @@ originurl = 'http://127.0.0.1:5000/visualization4'
 
 d3.json(originurl).then(function(response) {
     
-    console.log(response);
+    origins = Object.values(response.origin_name);
+    origins.forEach((origin) =>{
+        console.log(origin);
+    });
 
 })
