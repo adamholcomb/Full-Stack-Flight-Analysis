89f1b0d73c59b2b8d060b45ca986ce2b7d4a258b
Simon Castellanos <simoncastellanos9@gmail.com>
2021-08-13 09:32:38 -0500

    create logic3.js and add to html

---
+++ logic3.js
@@ -0,0 +1,11 @@
+// Javascript
+
+// Bubble Chart
+originurl = 'http://127.0.0.1:5000/visualization4'
+// bubbleurl = 'index.html/visualization3'
+
+d3.json(originurl).then(function(response) {
+    
+    console.log(response)
+
+})
