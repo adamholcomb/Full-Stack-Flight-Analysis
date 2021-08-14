5992566642a05ec00248bb858f560dc4b5cac060
adamholcomb <adam.holcomb@ymail.com>
2021-08-14 11:44:29 -0500

    Bubble chart code

--- logic2.js
+++ logic2.js
@@ -4,28 +4,113 @@
 bubbleurl = 'http://127.0.0.1:5000/visualization3'
 // bubbleurl = 'index.html/visualization3'
 
-d3.json(bubbleurl).then(function(response) {
+d3.json(bubbleurl).then(response => {
     console.log(response)
 
-    const config = {
-        type: 'bubble',
-        data: response,
-        options: {}
-    };
+    var flight_count = response['flight_count']
+    console.log(response[flight_count])
+    flightCount = Object.values(flight_count)
+    console.log(flightCount)
+
+    var min_cost = response['min_cost']
+    console.log(min_cost['Linear Air'])
+    minCost = Object.values(min_cost)
+    console.log(minCost)
+
+    carriers = Object.keys(min_cost)
+    console.log(carriers)
+
+    // xy = [{x:minCost,y:flightCount}]
+    // let combo = xy.map((item,index) => {`${index}:${item}`})
+    // console.log(combo)
+
+    const coords = flightCount.map((el, index)=> [el, minCost[index]]);
+    console.log(coords);
+
+
+    // let costs = response['flight_count'].map(count => {
+    //     return count['Linear Air']
+    // })
 
     const data = {
         datasets: [{
-        label: 'First Dataset',
-        data: [{
-            x: 20,
-            y: 30,
-            r: 15
-        }, {
-            x: 40,
-            y: 10,
-            r: 10
-        }],
-        backgroundColor: 'rgb(255, 99, 132)'
+            label: 'Flights',
+            labels: carriers,
+            data: coords,
+            backgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBackgroundColor: 'rgb(51, 204, 255)',
+            pointRadius: 10,
+            pointHoverRadius: 13,
         }]
     };
+
+    // const footer = (carriers) => {
+    //     // let label = carriers
+    //     carriers.forEach(carrier => {
+    //         return carrier;
+    //     });
+    //     // return carrier;
+    // };
+
+    const config = {
+        type: 'scatter',
+        data: data,
+        options: {
+            scales: {
+                x: {
+                  display: true,
+                  title: {
+                    display: true,
+                    text: 'Number of Flights',
+                    font: {size:14}
+                  }
+                },
+                y: {
+                  display: true,
+                  title: {
+                    display: true,
+                    text: 'Flight Cost',
+                    font: {size:14}
+                  }
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Cheapest flight data',
+                    color: 'rgb(255, 99, 132)',
+                    font: {size:22}
+                },
+                subtitle: {
+                    display: true,
+                    text: 'Plots the cheapest flight vs count of cheapest flights for each airline',
+                    color: 'rgb(51, 204, 255)',
+                    font: {
+                      size: 14,
+                      family: 'tahoma',
+                      weight: 'normal',
+                      style: 'italic'
+                    }
+                },
+                tooltip: {
+                    callbacks: {   
+                        label: function(ctx) {
+                            let label = ctx.dataset.labels[ctx.dataIndex];
+                            label += " (Flights: " + ctx.parsed.x + ", Cost: $" + ctx.parsed.y + ")";
+                            return label;
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+
+
+
+
 })
